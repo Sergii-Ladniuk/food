9729cf2372681394d7b105c4b74c16205da0b228
Sergii Ladniuk <sergii.ladniuk@gmail.com>
2014-04-11 17:26:16 +0200

    signup form validation p1

--- public/js/controllers/signupController.js
+++ public/js/controllers/signupController.js
@@ -2,14 +2,26 @@ define(['./module', '../app'], function (controllers) {
 	'use strict';
 	controllers.controller(
 		'SignupController',
-			function ($scope, $location) {
+			function ($scope, $location, UserService) {
+				$scope.submitted = false;
 				$scope.user = {};
+				$scope.passwordChanged = function() {
+					var passwordEl = angular.element('#password');
+					var confirmPasswordEl = angular.element('#confirmPassword');
+					confirmPasswordEl.$setValidity('match', passwordEl.getText() === confirmPasswordEl.getText());
+				};
+				$scope.verifyUser = function(){
+					if (UserService.isUserExists())
+				}
 				$scope.cancel = function () {
 					$location.path('/');
 				};
 				$scope.submit = function () {
-
-				}
+					$scope.submitted = true;
+					if ($scope.signupForm.$invalid) {
+						return;
+					}
+				};
 			}
 	);
 });
