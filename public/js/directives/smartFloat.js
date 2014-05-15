9c1d18d34a0a23b1db62c718cbc5cf3418c3b162
Sergii Ladniuk <sergii.ladniuk@gmail.com>
2014-05-15 19:25:53 +0300

    fix min/max constraints on product form

--- public/js/directives/smartFloat.js
+++ public/js/directives/smartFloat.js
@@ -6,16 +6,36 @@ define(['./directives'], function (directives) {
                 require: 'ngModel',
                 link: function ($scope, elm, attrs, ctrl) {
                     ctrl.$parsers.unshift(function (viewValue) {
-                        if (FLOAT_REGEXP.test(viewValue)) {
-                            ctrl.$setValidity('float', true);
-                            if (typeof viewValue === "number")
-                                return viewValue;
-                            else
-                                return parseFloat(viewValue.replace(',', '.'));
-                        } else {
-                            ctrl.$setValidity('float', false);
-                            return undefined;
+
+                        function verifyBounds(value) {
+                            if (value) {
+                                if (attrs.min) {
+                                    var min = parseFloat(attrs.min);
+                                    ctrl.$setValidity('min-bound', value >= min);
+                                }
+                                if (attrs.max) {
+                                    var max = parseFloat(attrs.max);
+                                    ctrl.$setValidity('max-bound', value <= max);
+                                }
+                            }
+                            return value;
                         }
+
+                        function verifyFloat() {
+                            if (FLOAT_REGEXP.test(viewValue)) {
+                                ctrl.$setValidity('float', true);
+                                if (typeof viewValue === "number") {
+                                    return viewValue;
+                                } else {
+                                    return parseFloat(viewValue.replace(',', '.'));
+                                }
+                            } else {
+                                ctrl.$setValidity('float', false);
+                                return undefined;
+                            }
+                        }
+
+                        return verifyBounds(verifyFloat());
                     });
                 }
             };
